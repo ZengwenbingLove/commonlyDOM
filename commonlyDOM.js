8da2f44b1698ca8ad6059dbac0daf3724de7d313
wjj <13524220689@189.cn>
2016-10-08 23:25:34 +0800

    event api finish

--- commonlyDOM.js
+++ commonlyDOM.js
@@ -1,5 +1,5 @@
 /**
-  @description 基于JavaScript实现CSS选择器选择DOM元素和集合一些常用的DOM属性\样式\Class操作、盒子读写、调整元素顺序、插入HTML\Text等方法
+  @description 基于JavaScript实现CSS选择器选择DOM元素和集合一些常用的DOM属性\样式\Class操作、盒子模型读写、调整元素顺序、插入HTML\Text等方法
 	@date 2016-10-7
 	@email zeng_wen_bing@sina.com
 **/
@@ -10,6 +10,7 @@
 	var prev$ = win.$;
 	var nativeSlice = Array.prototype.slice;
 	var nativeToString = Object.prototype.toString;
+  var nativeKeys = Object.keys;
 	var matchTagExp = /<([a-z]+)[^>]*?>(.*?)<\/\1>/i;
 	var matchSelectorExp = /^([.#]?)([\w-]+)$/i;
 
@@ -44,6 +45,73 @@
 			return [ likeArray ];
 		}
 	};
+
+  var each = function (entry, iteratee) {
+    var isArray = getType(entry) === 'array';
+    var collections = isArray ? entry : nativeKeys(entry);
+
+    for (var i = 0, length = collections.length; i < length; i++) {
+      var item = isArray ? collections[i] : entry[collections[i]];
+
+      if (iteratee.call(item, index, item, entry) === false) {
+        return;
+      }
+    }
+  };
+
+  var blankSplit = function (str) {
+    return str.trim().split(/\s+/);
+  }
+
+  var filterIndexRange = function (index, length) {
+    return index < 0
+     ? index += length
+     : length - 1;
+  };
+
+  var addEventListener = function (el, eventName, addHandler) {
+    if (getType(eventName) === 'string') {
+      eventName = blankSplit(eventName);
+    }
+
+    if (!getEvents(el)) {
+      el.eventListeners = {};
+    }
+
+    each(eventName, function (index, name) {
+      (getEvents(el)[name] || (getEvents(el)[name] = [])).push(addHandler);
+      el.addEventListener(name, addHandler, false);
+    });
+  };
+
+  var removeEventListener = function (el, eventName, removeHandle) {
+    if (!getEvents(el)) {
+      return;
+    }
+
+    var addedHandlers = getEvents(el)[eventName];
+    var handlers = clearEventListener(getEvents(el)[eventName], removeHandle);
+
+    getEvents(el)[eventName] = handlers;
+
+    each(addedHandlers, function (index, handler) {
+      if (handler === removeHandle) {
+        el.removeEventListener(eventName, handler);
+      }
+
+      return false;
+    });
+  };
+
+  var clearEventListener = function (handlers, removeHandler) {
+    return handlers.filter(function (handler, index) {
+      return handler !== removeHandler;
+    });
+  };
+
+  var getEvents = function (el) {
+    return el.eventListeners;
+  };
 
 	/**
    根据html创建element
@@ -133,13 +201,57 @@
 		return this.on('DOMContentLoaded', handle);
 	};
 
-	InitSelector.prototype.each = function () {
+	InitSelector.prototype.each = function (iteratee) {
+    $.each(this.dom, function (index, el) {
+      return iteratee.call(this, index, el);
+    });
 
+    return this;
 	};
+
+  InitSelector.prototype.get = function (index) {
+    if (getType(index) === 'undefined') {
+      return this.dom;
+    }
+
+    index = filterIndexRange(index);
+
+    return this.dom[index];
+  };
+
+  InitSelector.prototype.index = function (index) {
+    var constructor = this.constructor;
+
+    if (getType(index) === 'undefined') {
+      return new constructor(this.dom);
+    }
+
+    index = filterIndexRange(index);
+
+    return new constructor(this.dom[index]);
+  };
+
+  InitSelector.prototype.bind = InitSelector.prototype.on = function (eventName, addHandler) {
+    this.each(this.dom, function (index, el) {
+      addEventListener(el, eventName, addHandler);
+    });
+
+    return this;
+  };
+
+  InitSelector.prototype.unbind = InitSelector.prototype.off = function () {
+    this.each(this.dom, function (index, el) {
+      removeEventListener(el, eventName, addHandler);
+    });
+
+    return this;
+  };
 
 	function $ (selector, context) {
     return new InitSelector(selector, context);
 	}
+
+  $.each = each;
 
 	$.ready = function () {
 		var args = toArray(arguments);
